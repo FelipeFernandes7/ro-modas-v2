2ba3749ee7897d6c4e6a8451dbe67f9dc138449a
Luiz Felipe <71227873+FelipeFernandes7@users.noreply.github.com>
2024-02-17 18:38:27 -0300

    ajudando tamanho

--- src/components/header/sidebar/index.tsx
+++ src/components/header/sidebar/index.tsx
@@ -18,7 +18,7 @@ export function Sidebar({
   children,
 }: SidebarProps) {
   const styles = {
-    width: !openSidebar ? "0" : "240px",
+    width: !openSidebar ? "0" : "350px",
     transition: "width 0.3s ease-in-out",
   };
   return (
